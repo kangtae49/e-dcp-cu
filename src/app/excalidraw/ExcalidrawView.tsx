349e8d52d5a14fca662bcd910c3f1f072da90fff
kangtae49 <kangtae49@gmail.com>
2026-01-16 16:07:59 +0900

    ecalidraw add watch file

--- src/app/excalidraw/ExcalidrawView.tsx
+++ src/app/excalidraw/ExcalidrawView.tsx
@@ -3,27 +3,35 @@ import "@excalidraw/excalidraw/index.css";
 import {observer} from "mobx-react-lite";
 import {JustId} from "@/app/components/just-layout/justLayout.types.ts";
 import {Excalidraw, MainMenu} from "@excalidraw/excalidraw";
-import {AppState, BinaryFiles} from "@excalidraw/excalidraw/types";
+import {AppState, BinaryFiles, ExcalidrawImperativeAPI} from "@excalidraw/excalidraw/types";
 import {OrderedExcalidrawElement} from "@excalidraw/excalidraw/element/types";
 import {useExcalidrawStore} from "@/app/excalidraw/useExcalidrawStore.ts";
 import {JustUtil} from "@/app/components/just-layout/justUtil.ts";
-import {useState} from "react";
+import {useEffect, useLayoutEffect, useRef, useState} from "react";
 import {toJS} from "mobx";
 import {restoreAppState} from "@excalidraw/excalidraw";
 import {FontAwesomeIcon as Icon} from "@fortawesome/react-fontawesome"
 import {faExpand} from "@fortawesome/free-solid-svg-icons";
 import {useJustLayoutStore} from "@/app/components/just-layout/useJustLayoutStore.ts";
+import {useDrop} from "react-dnd";
+import {NativeTypes} from "react-dnd-html5-backend";
+import {FileItem} from "@/types.ts";
+import {useExcalidrawDataStore} from "@/app/excalidraw-data/useExcalidrawDataStore.ts";
+import {EXCALIDRAW_DATA_ID} from "@/app/excalidraw-data/excalidrawData.constants.ts";
 
 interface Props {
   justId: JustId
   layoutId: string
 }
 const ExcalidrawView = observer(({justId, layoutId}: Props) => {
+  const excalidrawRef = useRef<ExcalidrawImperativeAPI>(null);
+  const excalidrawDataStore = useExcalidrawDataStore(EXCALIDRAW_DATA_ID)
+
   const justLayoutStore = useJustLayoutStore(layoutId);
   const excalidrawStore = useExcalidrawStore(JustUtil.toString(justId))
   const [isFullScreen, setIsFullScreen] = useState(false)
-
-
+  const ref = useRef<HTMLDivElement>(null)
+  const [dataKey, setDataKey] = useState<string | null>(null)
   const changeFullScreen = async () => {
     setIsFullScreen(await window.api.isFullScreen())
   }
@@ -34,6 +42,7 @@ const ExcalidrawView = observer(({justId, layoutId}: Props) => {
     if (!appState) {
       return restoreAppState({}, null) as AppState
     }
+
     return restoreAppState({
       toast: appState.toast,
       fileHandle: appState.fileHandle,
@@ -70,14 +79,44 @@ const ExcalidrawView = observer(({justId, layoutId}: Props) => {
     // console.log('appState', appState)
     const strState = JSON.stringify(toJS({elements, appState: filterAppState(appState), files}))
     const strStoreState = JSON.stringify(toJS({elements: excalidrawStore.elements, appState: filterAppState(excalidrawStore.appState), files: excalidrawStore.files}))
-
     if (strState === strStoreState) return;
-    console.log("handleChange", elements, appState, files)
+    // if (appState.fileHandle) {
+    //   const filePath = window.api.getPathForFile(await appState.fileHandle.getFile())
+    //   console.log(filePath)
+    // }
+    // console.log("handleChange", elements, appState, files)
     excalidrawStore.setState({elements, appState: filterAppState(appState), files})
   }
+
+  const [, drop] = useDrop(() => ({
+    accept: [NativeTypes.FILE],
+    drop(_item: FileItem, monitor) {
+      const fileItem = monitor.getItem<FileItem>()
+      const filePath = window.api.getPathForFile(fileItem.files[0])
+      console.log(filePath)
+      window.api.addWatchPath([filePath])
+      setDataKey(filePath)
+    }
+  }), [])
+
+  useLayoutEffect(() => {
+    if (ref.current) {
+      drop(ref);
+    }
+  }, [drop]);
+
+  useEffect(() => {
+    if (!excalidrawRef.current) return;
+    if (dataKey === null) return;
+    if (!excalidrawDataStore.excalidrawDataMap[dataKey]) return;
+    excalidrawRef.current.updateScene(excalidrawDataStore.excalidrawDataMap[dataKey].data)
+  }, [excalidrawDataStore.excalidrawDataMap?.[dataKey ?? '']])
+
   return (
-    <div className="excalidraw-view">
+    <div className="excalidraw-view" ref={ref}>
       <Excalidraw
+        excalidrawAPI={(api) => {excalidrawRef.current = api}}
+
         UIOptions={{
         //   dockedSidebarBreakpoint: 0
         }}
